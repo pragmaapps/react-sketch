19b2f2becfae8c8076b45dcd6bbaeef84c754110
Aaryan Saxena <aaryan.saxena@pragmaapps.com>
2023-08-03 10:29:36 +0530

    Changed line implementation from drag to click

--- src/freedrawline.js
+++ src/freedrawline.js
@@ -56,6 +56,7 @@ class FreeDrawLine extends FabricCanvasTool {
         canvas.renderAll();
         this.startPoint = null;
         this.objectAdd = true;
+        props.onLineAdded();
 
         // Reset starting point
         this.startPoint = null;
@@ -82,23 +83,23 @@ class FreeDrawLine extends FabricCanvasTool {
     let canvas = this._canvas;
     const { onLineAdded } = props;
     var pointer = canvas.getPointer(o.e);
-    if(this.startPoint.left === pointer.x && this.startPoint.top ===pointer.y){
-      canvas.remove(this.line);
-      canvas.remove(this.startPoint);
-      this.line = null;
-      this.startPoint = null;
-      return;
-    }
-    if(this.outside) onLineAdded();
-    if(this.startPoint !== null && this.line && !this.checkWithInBoundary(o)){
-      this.line.set({ 'x2': pointer.x, 'y2': pointer.y });
-      canvas.renderAll();
-      onLineAdded();  
-     canvas.remove(this.startPoint);
-     this.outside = false;
-    }
-    this.startPoint = null;
-    this.line = null;
+    // if(this.startPoint.left === pointer.x && this.startPoint.top ===pointer.y){
+    //   canvas.remove(this.line);
+    //   canvas.remove(this.startPoint);
+    //   this.line = null;
+    //   this.startPoint = null;
+    //   return;
+    // }
+    // if(this.outside) onLineAdded();
+    // if(this.startPoint !== null && this.line && !this.checkWithInBoundary(o)){
+    //   this.line.set({ 'x2': pointer.x, 'y2': pointer.y });
+    //   canvas.renderAll();
+    //   onLineAdded();  
+    //  canvas.remove(this.startPoint);
+    //  this.outside = false;
+    // }
+    // this.startPoint = null;
+    // this.line = null;
   }
 
   checkWithInBoundary = (o) =>{
