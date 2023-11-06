dc3d659583504e2e6b1d4c32f9496a853f763d82
vishal.bhatia <vishal.bhatia@pragmaapps.com>
2023-11-06 11:04:34 +0530

    Added rotate icon for mtr control

--- src/nVisionSketchField.jsx
+++ src/nVisionSketchField.jsx
@@ -25,6 +25,36 @@ let controlsVisible = {
 let executeCanvasResize = false;
 fabric.Object.prototype.noScaleCache = false;
 //fabric.Object.prototype.setControlsVisibility(controlsVisible);
+var svgData = '<svg xmlns="http://www.w3.org/2000/svg" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch" viewBox="0 0 24 24" focusable="false" aria-hidden="true" data-testid="Rotate90DegreesCwIcon"><path fill="red" d="M4.64 19.37c3.03 3.03 7.67 3.44 11.15 1.25l-1.46-1.46c-2.66 1.43-6.04 1.03-8.28-1.21-2.73-2.73-2.73-7.17 0-9.9C7.42 6.69 9.21 6.03 11 6.03V9l4-4-4-4v3.01c-2.3 0-4.61.87-6.36 2.63-3.52 3.51-3.52 9.21 0 12.73zM11 13l6 6 6-6-6-6-6 6z"></path></svg>';
+
+var rotateIcon = 'data:image/svg+xml,' + encodeURIComponent(svgData);
+var img = document.createElement('img');
+img.src = rotateIcon;
+
+// here's where your custom rotation control is defined
+// by changing the values you can customize the location, size, look, and behavior of the control
+fabric.Object.prototype.controls.mtr = new fabric.Control({
+  x: 0,
+  y: -0.5,
+  offsetY: -40,
+  cursorStyle: 'crosshair',
+  actionHandler: fabric.controlsUtils.rotationWithSnapping,
+  actionName: 'rotate',
+  render: renderIcon,
+  cornerSize: 16,
+  withConnection: true
+});
+
+// here's where the render action for the control is defined
+function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+  var size = this.cornerSize;
+  ctx.save();
+  ctx.translate(left, top);
+  ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+  ctx.drawImage(img, -size / 2, -size / 2, size, size);
+  ctx.restore();
+}
+
 fabric.Object.prototype.set({
   cornerSize: 6,
   cornerColor : 'red',
