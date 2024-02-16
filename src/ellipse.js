c93899e6eb44ff65c8204d79d813a2d41a56fb94
aaryan.saxena <aaryan.saxena@pragmaapps.com>
2024-02-16 12:36:06 +0530

    2.4.0-alpha1: Remove the code of trigger type and triggerEvent for nvision closed loop

--- src/ellipse.js
+++ src/ellipse.js
@@ -79,7 +79,6 @@ class Ellipse extends FabricCanvasTool {
       strokeUniform: true,
       centerPoint:{x:0,y:0}
     });
-    this.addEventTriggerKeys();
     canvas.add(this.ellipse);
     this.ellipse.setCoords();
     // this.containInsideBoundary(options);
@@ -88,10 +87,6 @@ class Ellipse extends FabricCanvasTool {
     removeColorInDefaultShapeColors(props.defaultShapeColors);
     addROIDefaultName(props.roiDefaultNames);
   };
-  addEventTriggerKeys = ()=>{
-    this.ellipse.triggerEvent= "none";
-    this.ellipse.triggerType= "none";
-  }
 
   doMouseMove(o, props) {
     if (!this.isDown) return;
